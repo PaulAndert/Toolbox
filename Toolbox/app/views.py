285b88e91bf3df6680783f87dd4f38b7debdc8b7
PaulAndert <72561048+PaulAndert@users.noreply.github.com>
2022-05-18 17:30:39 +0200

    Update views.py

--- Toolbox/app/views.py
+++ Toolbox/app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from app.models import *
+
+
+class ApplicationList(ListView):
+    model = Application
+
+class ApplicationView(DetailView):
+    model = Application
