0c1e26846da4f27897e12ce5ed8d0b15de061483
PaulAndert <72561048+PaulAndert@users.noreply.github.com>
2022-05-18 17:30:10 +0200

    Update models.py

--- Toolbox/app/models.py
+++ Toolbox/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Application(models.Model):
+    class Meta:
+        verbose_name_plural = 'Applications'
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+    functionname = models.CharField(max_length=50)
+
