8bc449a584dd0eaea00be1825a3c992b75c96939
kekolyt <Mp2002-toni>
2022-05-24 11:08:35 +0200

    reworked htmls

--- Toolbox/Toolbox/app/views.py
+++ Toolbox/Toolbox/app/views.py
@@ -11,5 +11,22 @@ from app.models import *
 class ApplicationList(ListView):
     model = Application
 
+
 class ApplicationView(DetailView):
     model = Application
+
+
+def binaryToDecimal(number):
+    return int(number, 2)
+
+
+def decimalToBinary(number):
+    return bin(number)
+
+
+def octalToDecimal(number):
+    return int(number)
+
+
+def decimalToOctal(decimal_num):
+    return oct(decimal_num)
