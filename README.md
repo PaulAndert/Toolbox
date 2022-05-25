57cc8f2197319c8d5ea38929943bb796635c3903
PaulAndert <72561048+PaulAndert@users.noreply.github.com>
2022-05-25 15:18:27 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Toolbox
+Tools:  
+- RGB Converter
+- Binary Calculator
+- Hexadezimal Calculator
+- Timer/ Countdown
+- normal Calculator
+- radiostream downloader
+- 
---
+++ Toolbox/app/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from app.models import *
+
+
+class ApplicationList(ListView):
+    model = Application
+
+
+class ApplicationView(DetailView):
+    model = Application
+
+
+def binaryToDecimal(number):
+    return int(number, 2)
+
+
+def decimalToBinary(number):
+    return bin(number)
+
+
+def octalToDecimal(number):
+    return int(number)
+
+
+def decimalToOctal(decimal_num):
+    return oct(decimal_num)
+
+def decimalToHexa(num):
+    return hex(num)
+
+def hexaToDecimal(hexa_num):
+    return int(hexa_num, 16)
+
+
