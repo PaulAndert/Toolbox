6340b285f5579a39eb5663b8aa413d374ce42961
MAPEST <MAPEST@bpe.local>
2022-05-25 18:15:21 +0200

    binaryToDecimal überarbeitet

--- Toolbox/app/views.py
+++ Toolbox/app/views.py
@@ -8,7 +8,6 @@ from django.views.generic import ListView, DetailView
 from app.models import *
 
 
-
 class ApplicationList(ListView):
     model = Application
 
@@ -19,26 +18,35 @@ class ApplicationView(DetailView):
 
 def binaryToDecimal(request):
     number = request.POST['bin']
-    result = int(number, 2)
+    try:
+        result = int(number, 2)
+    except:
+        result = "Geben Sie bitte eine Binärzahl ein"
     return render(request, 'app/binaryToDecimal.html', {'page_title': 'binaryToDecimal',
                                                         'result': result})
+
 
 def decimalToBinary(number):
     return bin(number)
 
+
 def octalToDecimal(number):
     return int(number, 8)
+
 
 def decimalToOctal(decimal_num):
     return oct(decimal_num)
 
+
 def decimalToHexa(num):
     return hex(num)
+
 
 def hexaToDecimal(hexa_num):
     return int(hexa_num, 16)
 
-def rgbToCmyk(r, g, b, CMYK_SCALE = 100, RGB_SCALE = 255):
+
+def rgbToCmyk(r, g, b, CMYK_SCALE=100, RGB_SCALE=255):
     if (r, g, b) == (0, 0, 0): return 0, 0, 0, CMYK_SCALE
     y = 1 - b / RGB_SCALE
     c = 1 - r / RGB_SCALE
@@ -50,16 +58,16 @@ def rgbToCmyk(r, g, b, CMYK_SCALE = 100, RGB_SCALE = 255):
     k = min_cmy
     return c * CMYK_SCALE, m * CMYK_SCALE, y * CMYK_SCALE, k * CMYK_SCALE
 
-def cmykToRgb(c, m, y, k, cmyk_scale = 100, rgb_scale=255):
+
+def cmykToRgb(c, m, y, k, cmyk_scale=100, rgb_scale=255):
     r = rgb_scale * (1.0 - c / float(cmyk_scale)) * (1.0 - k / float(cmyk_scale))
     g = rgb_scale * (1.0 - m / float(cmyk_scale)) * (1.0 - k / float(cmyk_scale))
     b = rgb_scale * (1.0 - y / float(cmyk_scale)) * (1.0 - k / float(cmyk_scale))
     return r, g, b
+
 
 def manage_functions(request, pk=None):
     if pk:
         function = Application.objects.get(pk=pk).name
 
     return render(request, f'app/{function}.html', {'page_title': f'{function}'})
-
-
