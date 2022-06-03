345c4f0d56fbed14c34023302f389dae97be4fd7
PaulAndert <paulandert@web.de>
2022-06-03 12:25:56 +0200

    added description to db and deleted the unused templates

--- Toolbox/app/views.py
+++ Toolbox/app/views.py
@@ -18,139 +18,6 @@ class ApplicationView(DetailView):
     model = Application
 
 
-def binaryToDecimal(request):
-    number = request.POST['bin']
-    try:
-        return render(request, 'app/binaryToDecimal.html',
-                      {'page_title': 'binaryToDecimal', 'op': 0, 'error': "",
-                       'result': int(number, 2), 'number': number})
-
-    except:
-        return render(request, 'app/binaryToDecimal.html',
-                      {'page_title': 'binaryToDecimal', 'op': 1, 'error': "Geben Sie bitte eine Binärzahl ein",
-                       'result': "", 'number': number})
-
-
-def decimalToBinary(request):
-    number = request.POST['bin']
-    try:
-        return render(request, 'app/decimalToBinary.html',
-                      {'page_title': 'decimalToBinary', 'op': 0, 'error': "",
-                       'result': bin(int(number)), 'number': number})
-    except:
-        return render(request, 'app/decimalToBinary.html',
-                      {'page_title': 'decimalToBinary', 'op': 1, 'error': "Geben Sie bitte eine Dezimalzahl ein",
-                       'result': "", 'number': number})
-
-
-def octalToDecimal(request):
-    number = request.POST['oct']
-    try:
-        return render(request, 'app/octalToDecimal.html',
-                      {'page_title': 'octalToDecimal', 'op': 0, 'error': "",
-                       'result': int(number, 8), 'number': number})
-    except:
-        return render(request, 'app/octalToDecimal.html',
-                      {'page_title': 'octalToDecimal', 'op': 1, 'error': "Geben Sie bitte eine Dezimalzahl ein",
-                       'result': "", 'number': number})
-
-
-def decimalToOctal(request):
-    number = request.POST['bin']
-    try:
-        result = oct(int(number))
-    except:
-        error = "Geben Sie bitte eine Dezimalzahl ein"
-        result = ""
-        return render(request, f'app/decimalToOctal.html',
-                      {'page_title': f'decimalToOctal', 'result': result, 'number': number, 'op': 1, 'error': error})
-    return render(request, f'app/decimalToOctal.html',
-                  {'page_title': f'decimalToOctal', 'result': result, 'number': number, "op": 0})
-
-
-def decimalToHexa(request):
-    number = request.POST['bin']
-    try:
-        result = hex(int(number))
-    except:
-        error = "Geben Sie bitte eine Dezimalzahl ein"
-        result = ""
-        return render(request, f'app/decimalToHexa.html',
-                      {'page_title': f'decimalToHexa', 'result': result, 'number': number, 'op': 1, 'error': error})
-    return render(request, f'app/decimalToHexa.html',
-                  {'page_title': f'decimalToHexa', 'result': result, 'number': number, 'op': 0})
-
-
-def hexaToDecimal(request):
-    number = request.POST['hex']
-    try:
-        result = int(number, 16)
-    except:
-        error = "Geben Sie bitte eine Hexadezimalzahl ein"
-        result = ""
-        return render(request, f'app/hexaToDecimal.html',
-                      {'page_title': f'hexaToDecimal', 'result': result, 'number': number, 'op': 1, 'error': error})
-    return render(request, f'app/hexaToDecimal.html',
-                  {'page_title': f'hexaToDecimal', 'result': result, 'number': number, 'op': 0})
-
-
-def rgbToCmyk(request, CMYK_SCALE=100, RGB_SCALE=255):
-    try:
-        r = int(request.POST['r'])
-        g = int(request.POST['g'])
-        b = int(request.POST['b'])
-    except:
-        return render(request, 'app/rgbToCmyk.html',
-                      {'page_title': f'rgbToCmyk', 'op': 1,
-                       'error': "Bitte geben sie 3 nummern zwischen 0 und 255 ein",
-                       'c': "", 'm': "", 'y': "", 'k': "", })
-
-    if not (0 <= r <= 255) or not (0 <= g <= 255) or not (0 <= b <= 255):
-        return render(request, f'app/rgbToCmyk.html',
-                      {'page_title': f'rgbToCmyk', 'op': 1, 'error': "Bitte nur nummern zwischen 0 und 255 nutzen",
-                       'c': "", 'm': "", 'y': "", 'k': "", })
-    if (r, g, b) == (0, 0, 0):
-        return render(request, f'app/rgbToCmyk.html',
-                      {'page_title': f'rgbToCmyk', 'op': 0, 'error': "", 'c': 0, 'm': 0, 'y': 0, 'k': CMYK_SCALE, })
-    y = 1 - b / RGB_SCALE
-    c = 1 - r / RGB_SCALE
-    m = 1 - g / RGB_SCALE
-    min_cmy = min(c, m, y)
-    c = (c - min_cmy) / (1 - min_cmy)
-    m = (m - min_cmy) / (1 - min_cmy)
-    y = (y - min_cmy) / (1 - min_cmy)
-    k = min_cmy
-    return render(request, f'app/rgbToCmyk.html',
-                  {'page_title': f'rgbToCmyk', 'op': 0, 'error': "", 'c': c * CMYK_SCALE, 'm': m * CMYK_SCALE,
-                   'y': y * CMYK_SCALE, 'k': k * CMYK_SCALE, })
-
-
-def cmykToRgb(request, cmyk_scale=100, rgb_scale=255):
-    try:
-        c = float(request.POST['c'])
-        m = float(request.POST['m'])
-        y = float(request.POST['y'])
-        k = float(request.POST['k'])
-
-    except:
-        return render(request, 'app/cmykToRgb.html',
-                      {'page_title': f'cmykToRgb', 'op': 1,
-                       'error': "Bitte geben sie 4 nummern zwischen 0 und 100 ein",
-                       'r': "", 'g': "", 'b': "", })
-    if not (0 <= c <= 100) or not (0 <= m <= 100) or not (0 <= y <= 100) or not (0.0 <= k <= 1.0):
-        return render(request, f'app/cmykToRgb.html',
-                      {'page_title': f'cmykToRgb', 'op': 1,
-                       'error': "Bitte nur nummern zwischen 0 und 100 nutzen bzw. bei kontrolle nur von 0 bis 1",
-                       'r': "", 'g': "", 'b': "", })
-
-    r = rgb_scale * (1.0 - c / float(cmyk_scale)) * (1.0 - k / float(cmyk_scale))
-    g = rgb_scale * (1.0 - m / float(cmyk_scale)) * (1.0 - k / float(cmyk_scale))
-    b = rgb_scale * (1.0 - y / float(cmyk_scale)) * (1.0 - k / float(cmyk_scale))
-    return render(request, f'app/cmykToRgb.html',
-                  {'page_title': f'cmykToRgb', 'op': 0, 'error': "",
-                   'r': r, 'g': g, 'b': b, })
-
-
 def manage_functions(request, pk=None):
     if pk:
         function = Application.objects.get(pk=pk)
@@ -188,7 +55,8 @@ def select_function(request, pk=None):
             val = val.replace('{{ ' + alphabet[i] + ' }}', str(loc[alphabet[i]]))
 
         return render(request, f'app/template.html',
-                      {'page_title': f'{function.name}', 'id': f'{pk}', 'templatecode': val, 'description': function.description,})
+                      {'page_title': f'{function.name}', 'id': f'{pk}', 'templatecode': val,
+                       'description': function.description, })
     except:
         val = function.templatetext.replace('{{ opci }}', '1').replace('{{ error }}', function.errormessage)
         for i in range(1, function.outputanzahl + 1):
@@ -212,6 +80,7 @@ code = ''
 error = ''
 desc = ''
 
+
 def next_app(request):
     templatetext = ''
     global appname
@@ -232,7 +101,7 @@ def next_app(request):
     out_str = '<p>Geben sie num. output Variable: <input type="text" name="{{ name }}out"></p>'
 
     for x in range(1, input_variable + 1):
-        templatetext += in_str.replace('num', str(x)).replace('{{ name }}', alphabet[x])\
+        templatetext += in_str.replace('num', str(x)).replace('{{ name }}', alphabet[x]) \
             .replace('{{ type }}', alphabet[x] + 'type')
     templatetext += '<br>'
     for y in range(1, output_variable + 1):
